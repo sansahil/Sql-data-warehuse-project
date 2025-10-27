aa94364770387c018ccb4c251175b4ccc5aa230e
78sahil <145114926+sansahil@users.noreply.github.com>
2025-10-27 11:37:15 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -46,8 +46,8 @@ cid NVARCHAR(50),
 bdate DATE,
 gen NVARCHAR(50)
 );
-IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
-  DROP TABLE bronze.erp_cust_az12;
+IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+  DROP TABLE ;bronze.erp_px_cat_g1v2
 CREATE TABLE bronze.erp_px_cat_g1v2(
 id NVARCHAR(50),
 cat NVARCHAR(50),
